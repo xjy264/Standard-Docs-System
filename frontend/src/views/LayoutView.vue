c40d8a10dabacef60d7980b1eab8553f2cd6ca2f
xjy264 <2642630566@qq.com>
2026-06-03 15:36:57 +0800

    fix: adjust console navigation links

--- frontend/src/views/LayoutView.vue
+++ frontend/src/views/LayoutView.vue
@@ -4,7 +4,7 @@
       <div class="top-actions top-left"></div>
       <div class="brand">标准化资料管理系统</div>
       <div class="top-actions top-right">
-        <el-button class="console-button" plain @click="router.push('/org')">资料目录</el-button>
+        <el-button class="console-button" plain @click="router.push('/dashboard')">资料目录</el-button>
         <el-button class="console-button" plain @click="router.push('/console/personal')">控制台</el-button>
         <span>{{ auth.user?.realName || auth.user?.phone }}</span>
         <el-button link style="color:#fff;margin-left:14px" @click="logout">退出</el-button>
@@ -13,7 +13,6 @@
     <main class="workspace">
       <aside class="sidebar">
         <el-menu router :default-active="$route.path" :default-openeds="defaultOpeneds">
-          <el-menu-item index="/dashboard">首页</el-menu-item>
           <template v-if="isConsole">
             <el-menu-item index="/console/personal">个人空间</el-menu-item>
             <el-menu-item v-if="canManageDocRoots" index="/console/doc-root-folders">资料目录设置</el-menu-item>
@@ -21,6 +20,7 @@
             <el-menu-item v-if="auth.hasPermission('user:view')" index="/console/users">用户管理</el-menu-item>
           </template>
           <template v-else>
+            <el-menu-item index="/dashboard">首页</el-menu-item>
             <el-menu-item v-for="dept in navigation" :key="dept.id" :index="`/org/${dept.id}`">{{ dept.deptName }}</el-menu-item>
           </template>
         </el-menu>
